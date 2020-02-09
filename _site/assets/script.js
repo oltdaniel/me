e1e063db0ab38652c0b4c17834e4f43ddcead7fd
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-02-09 20:40:17 +0100

    change deploy structure

--- _site/assets/script.js
+++
@@ -1,2 +0,0 @@
-// Copyright (C) 2020  Daniel Oltmanns
-function r(u) {window.location.href=u}
--- pu.sh
+++ pu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed 's/future: true/future: false/g' _config.yml > _config.deploy.yml
-bundle exec jekyll b --config _config.deploy.yml
+sed 's/future: true/future: false/g' _config.dev.yml > _config.yml
+bundle exec jekyll b
 for f in $(find _site -name "*.html" -type f); do
     htmlbeautifier "$f"
     echo "$f"
