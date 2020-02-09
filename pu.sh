663b784f12050d42243a8fb8495fdec0d3725404
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-02-09 20:34:25 +0100

    add nav and prepare portfolio
    site will now be uploaded pre-compiled in _site

--- pu.sh
+++ pu.sh
@@ -2,8 +2,7 @@
 
 sed 's/future: true/future: false/g' _config.yml > _config.deploy.yml
 bundle exec jekyll b --config _config.deploy.yml
-for f in `find _site -name "*.html" -type f`; do
-    htmlbeautifier $f
-    echo $f
+for f in $(find _site -name "*.html" -type f); do
+    htmlbeautifier "$f"
+    echo "$f"
 done
-scp -r _site/* root@oltdaniel.at:/var/www/oltdaniel.at/
