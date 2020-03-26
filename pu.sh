674b128405238ded7500369a303c6037fcb3106c
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-03-26 19:05:54 +0100

    Delete pu.sh

--- pu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed 's/future: true/future: false/g' _config.dev.yml > _config.yml
-bundle exec jekyll b
-for f in $(find _site -name "*.html" -type f); do
-    htmlbeautifier "$f"
-    echo "$f"
-done
