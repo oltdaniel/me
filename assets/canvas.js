0141c9009771273cafe66c84e188a87d54050075
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-03-31 10:53:21 +0200

    added chart scale

--- assets/canvas.js
+++ assets/canvas.js
@@ -5,6 +5,16 @@ const DEFAULT_OPTS = {
     maxY: 100, // max x value
 };
 
+function findMax(data) {
+    let maxValue = data[0];
+    for(let i = 1; i < data.length; i++) {
+        if(data[i] > maxValue) {
+            maxValue = data[i];
+        }
+    }
+    return maxValue;
+}
+
 function Chart(id, opts = DEFAULT_OPTS) {
     let el = document.getElementById(id);
     this.canvas = el;
@@ -14,8 +24,9 @@ function Chart(id, opts = DEFAULT_OPTS) {
         minY: 0, // min y value
         maxY: 100, // max x value
     };
-    this.opts.height = el.getAttribute('height');
-    this.opts.width = el.getAttribute('width');
+    this.opts.sideAxisOffset = (el.getAttribute('width') / 100) * 2;
+    this.opts.height = el.getAttribute('height') - this.opts.sideAxisOffset;
+    this.opts.width = el.getAttribute('width') - (this.opts.sideAxisOffset * 2);
     this.calculatePxValues();
 }
 
@@ -23,11 +34,122 @@ Chart.prototype.drawData = function(data, color) {
     let c = this.canvas.getContext('2d');
     c.beginPath();
     c.strokeStyle = color;
-    c.lineWidth = 1.5;
-    c.moveTo(0,this.opts.height - data[0] * this.opts.pxPerValue);
+    c.lineWidth = 2;
+    c.moveTo(this.opts.sideAxisOffset,this.opts.height - data[0] * this.opts.pxPerValue);
     for(let i = 1; i < data.length; i++) {
-        c.lineTo(i * this.opts.pxPerDay, this.opts.height - data[i] * this.opts.pxPerValue);
+        c.lineTo(this.opts.sideAxisOffset + ((i + 1) * this.opts.pxPerDay), this.opts.height - data[i] * this.opts.pxPerValue);
         c.stroke();
+    }
+};
+
+Chart.prototype.drawGrid = function(data) {
+    let count = data.lines.length;
+    let c = this.canvas.getContext('2d');
+    c.beginPath();
+    c.strokeStyle = '#ddd';
+    for(let i = 1; i < count / 7; i++) {
+        c.lineWidth = 1;
+        c.moveTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7),0);
+        c.lineTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7),this.opts.height);
+        c.stroke();
+    }
+    c.beginPath();
+    for(let i = 1; i < 10; i++) {
+        let height = (this.opts.height / 10) * i;
+        c.lineWidth = 1;
+        c.moveTo(this.opts.sideAxisOffset,height);
+        c.lineTo(this.opts.sideAxisOffset + this.opts.width, height);
+        c.stroke();
+    }
+};
+
+CanvasRenderingContext2D.prototype.fillVerticalText = function(text, x, y, verticalSpacing) {
+    for (let i = 0; i < text.length; i++) {
+        this.fillText(text[i], x, y + i * verticalSpacing);
+    }
+};
+
+String.prototype.intWithCommas = function() {
+    return this.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+Chart.prototype.drawLeftAxis = function(data) {
+    let max = findMax(data.allValuesByHeader('confirmed'));
+    let low = 0;
+    let c = this.canvas.getContext('2d');
+    c.strokeStyle = '#212121';
+    c.font = "bold 25px Courier New";
+    c.textAlign = 'left';
+    c.beginPath();
+    c.moveTo(this.opts.sideAxisOffset, 0);
+    c.lineTo(this.opts.sideAxisOffset, this.opts.height);
+    c.stroke();
+    c.moveTo(this.opts.sideAxisOffset - 10, 5);
+    c.lineTo(this.opts.sideAxisOffset, 5);
+    c.stroke();
+    c.moveTo(this.opts.sideAxisOffset - 10, this.opts.height - 5);
+    c.lineTo(this.opts.sideAxisOffset, this.opts.height - 5);
+    c.stroke();
+    c.fillText(String(low), 2, this.opts.height);
+    c.fillVerticalText(String(max).intWithCommas(), 0, 20, 23);
+};
+
+Chart.prototype.drawRightAxis = function(data) {
+    let max = findMax(data.allValuesByHeader('difference'));
+    let low = 0;
+    let c = this.canvas.getContext('2d');
+    c.strokeStyle = '#212121';
+    c.font = "bold 25px Courier New";
+    c.textAlign = 'right';
+    c.beginPath();
+    let widthLimit = this.opts.width + (this.opts.sideAxisOffset * 2);
+    c.moveTo(widthLimit - this.opts.sideAxisOffset, 0);
+    c.lineTo(widthLimit - this.opts.sideAxisOffset, this.opts.height);
+    c.stroke();
+    c.moveTo(widthLimit - this.opts.sideAxisOffset, 5);
+    c.lineTo(widthLimit - this.opts.sideAxisOffset + 10, 5);
+    c.stroke();
+    c.moveTo(widthLimit - this.opts.sideAxisOffset, this.opts.height - 5);
+    c.lineTo(widthLimit - this.opts.sideAxisOffset + 10, this.opts.height - 5);
+    c.stroke();
+    c.fillText(String(low), widthLimit, this.opts.height);
+    c.fillVerticalText(String(max).intWithCommas(), widthLimit, 20, 23);
+};
+
+Chart.prototype.drawBottomAxis = function(data) {
+    let max = new Date(data.lines[data.lines.length - 1][0]);
+    let low = new Date(data.lines[0][0]);
+    max = '' + max.getDate() + '.' + (max.getMonth() + 1);
+    low = '' + low.getDate() + '.' + (low.getMonth() + 1);
+    let c = this.canvas.getContext('2d');
+    c.strokeStyle = '#212121';
+    c.font = "bold 25px Courier New";
+    c.beginPath();
+    c.moveTo(this.opts.sideAxisOffset, this.opts.height);
+    c.lineTo(this.opts.sideAxisOffset + this.opts.width, this.opts.height);
+    c.stroke();
+    c.moveTo(this.opts.sideAxisOffset + this.opts.width - 5, this.opts.height);
+    c.lineTo(this.opts.sideAxisOffset + this.opts.width - 5, this.opts.height + 5);
+    c.stroke();
+    c.moveTo(this.opts.sideAxisOffset + 5, this.opts.height);
+    c.lineTo(this.opts.sideAxisOffset + 5, this.opts.height + 5);
+    c.stroke();
+    c.textAlign = 'left';
+    c.fillText(low, this.opts.sideAxisOffset / 2, this.opts.height + this.opts.sideAxisOffset);
+    c.textAlign = 'right';
+    c.fillText(max, this.opts.sideAxisOffset + this.opts.width, this.opts.height + this.opts.sideAxisOffset);
+    c.beginPath();
+    c.textAlign = 'center';
+    for(let i = 1; i < data.lines.length / 7; i++) {
+        if(data.lines.length - (i * 7) < 6) {
+            continue;
+        }
+        let cur = new Date(data.lines[i * 7][0]);
+        cur = '' + cur.getDate() + '.' + (cur.getMonth() + 1);
+        c.moveTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7), this.opts.height);
+        c.lineTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7), this.opts.height + 10);
+        c.stroke();
+        c.fillText(cur, this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7), this.opts.height + this.opts.sideAxisOffset);
     }
 };
 
