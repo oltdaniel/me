4b4c28f4621b1793300a0ac1c413a8c3b0361428
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-04-02 01:31:29 +0200

    comments, remove debug

--- assets/canvas.js
+++ assets/canvas.js
@@ -23,8 +23,9 @@ function Chart(id, opts = DEFAULT_OPTS) {
         drawDots: false, // on data entry
         minY: 0, // min y value
         maxY: 100, // max x value
+        sideOffsetPercentage: 2.5, // percentage of space on each side of width
     };
-    this.opts.sideAxisOffset = (el.getAttribute('width') / 100) * 2.5;
+    this.opts.sideAxisOffset = (el.getAttribute('width') / 100) * this.opts.sideOffsetPercentage;
     this.opts.height = el.getAttribute('height') - this.opts.sideAxisOffset;
     this.opts.width = el.getAttribute('width') - (this.opts.sideAxisOffset * 2);
     this.calculatePxValues();
@@ -35,7 +36,9 @@ Chart.prototype.drawData = function(data, color) {
     c.beginPath();
     c.strokeStyle = color;
     c.lineWidth = 2;
+    // starting point right of the left axis
     c.moveTo(this.opts.sideAxisOffset,this.opts.height - data[0] * this.opts.pxPerValue);
+    // continue to draw each datapoint with a tail line
     for(let i = 1; i < data.length; i++) {
         c.lineTo(this.opts.sideAxisOffset + ((i + 1) * this.opts.pxPerDay), this.opts.height - data[i] * this.opts.pxPerValue);
         c.stroke();
@@ -47,12 +50,14 @@ Chart.prototype.drawGrid = function(data) {
     let c = this.canvas.getContext('2d');
     c.beginPath();
     c.strokeStyle = '#ddd';
+    // draw weekly horizontal line
     for(let i = 1; i < count / 7; i++) {
         c.lineWidth = 1;
         c.moveTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7),0);
         c.lineTo(this.opts.sideAxisOffset + (i * this.opts.pxPerDay * 7),this.opts.height);
         c.stroke();
     }
+    // draw 10% vertical line
     c.beginPath();
     for(let i = 1; i < 10; i++) {
         let height = (this.opts.height / 10) * i;
@@ -73,8 +78,9 @@ String.prototype.intWithCommas = function() {
     return this.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
+// shortcut
 Number.prototype.intWithCommas = function() {
-    return String(this).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return String(this).intWithCommas();
 };
 
 Chart.prototype.drawLeftAxis = function(data) {
@@ -85,15 +91,18 @@ Chart.prototype.drawLeftAxis = function(data) {
     c.font = "bold 25px Courier New";
     c.textAlign = 'left';
     c.beginPath();
+    // draw axis
     c.moveTo(this.opts.sideAxisOffset, 0);
     c.lineTo(this.opts.sideAxisOffset, this.opts.height);
     c.stroke();
+    // draw indicator lines
     c.moveTo(this.opts.sideAxisOffset - 10, 5);
     c.lineTo(this.opts.sideAxisOffset, 5);
     c.stroke();
     c.moveTo(this.opts.sideAxisOffset - 10, this.opts.height - 5);
     c.lineTo(this.opts.sideAxisOffset, this.opts.height - 5);
     c.stroke();
+    // write text
     c.fillText(String(low), 2, this.opts.height);
     c.fillVerticalText(String(max).intWithCommas(), 0, 20, 23);
     c.font = "bold 15px Courier New";
@@ -110,15 +119,18 @@ Chart.prototype.drawRightAxis = function(data) {
     c.textAlign = 'right';
     c.beginPath();
     let widthLimit = this.opts.width + (this.opts.sideAxisOffset * 2);
+    // draw axis
     c.moveTo(widthLimit - this.opts.sideAxisOffset, 0);
     c.lineTo(widthLimit - this.opts.sideAxisOffset, this.opts.height);
     c.stroke();
+    // draw indicator lines
     c.moveTo(widthLimit - this.opts.sideAxisOffset, 5);
     c.lineTo(widthLimit - this.opts.sideAxisOffset + 10, 5);
     c.stroke();
     c.moveTo(widthLimit - this.opts.sideAxisOffset, this.opts.height - 5);
     c.lineTo(widthLimit - this.opts.sideAxisOffset + 10, this.opts.height - 5);
     c.stroke();
+    // write text
     c.fillText(String(low), widthLimit, this.opts.height);
     c.fillVerticalText(String(max).intWithCommas(), widthLimit, 20, 23);
 };
@@ -132,19 +144,23 @@ Chart.prototype.drawBottomAxis = function(data) {
     c.strokeStyle = '#212121';
     c.font = "bold 25px Courier New";
     c.beginPath();
+    // draw axis
     c.moveTo(this.opts.sideAxisOffset, this.opts.height);
     c.lineTo(this.opts.sideAxisOffset + this.opts.width, this.opts.height);
     c.stroke();
+    // draw indicator lines
     c.moveTo(this.opts.sideAxisOffset + this.opts.width - 5, this.opts.height);
     c.lineTo(this.opts.sideAxisOffset + this.opts.width - 5, this.opts.height + 5);
     c.stroke();
     c.moveTo(this.opts.sideAxisOffset + 5, this.opts.height);
     c.lineTo(this.opts.sideAxisOffset + 5, this.opts.height + 5);
     c.stroke();
+    // write text
     c.textAlign = 'left';
     c.fillText(low, this.opts.sideAxisOffset / 2, this.opts.height + this.opts.sideAxisOffset);
     c.textAlign = 'right';
     c.fillText(max, this.opts.sideAxisOffset + this.opts.width, this.opts.height + this.opts.sideAxisOffset);
+    // create date points on axis (for every full week, else text overlays)
     c.beginPath();
     c.textAlign = 'center';
     for(let i = 1; i < data.lines.length / 7; i++) {
